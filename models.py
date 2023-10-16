1f34f7172bef09a96cb26161fc06607df47c88fd
JanDore7 <jan_dore2030@proton.me>
2023-10-17 01:19:13 +0300

    Развернули БД

---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, Column, String, DateTime, func, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+PG_USER = 'app'
+PG_PASSWORD = '1234'
+PG_DB = 'app'
+PG_HOST = '127.0.0.1'
+PG_PORT = 5431
+
+PG_DSN = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}'
+engin = create_engine(PG_DSN)
+
+Session = sessionmaker(bind=engin)
+Base = declarative_base(bind=engin)
+
+
+class User(Base):
+
+    __tablename__ = 'app_users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True, index=True)
+    password = Column(String, nullable=False)
+    creation_time = Column(DateTime, server_default=func.now())
+
+
+Base.metadata.create_all()
