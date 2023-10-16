b3d4f5419b727e24b81e1f94ce6406b5c2376ae5
JanDore7 <jan_dore2030@proton.me>
2023-10-17 00:11:46 +0300

    Принимаем request от пользователя

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests
 
-response = requests.post('http://127.0.0.1:5000//hello/world')
+response = requests.post('http://127.0.0.1:5000//hello/world/555', json={'json_key': 'value'},
+                         headers={'token': 'secret'},
+                         params={'k1':'v10', 'k2': 'v20'})
 print(response.status_code)
 print(response.text)
--- server.py
+++ server.py
@@ -1,16 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask('app')
 
 
-def hello_world():
+def hello_world(some_id):
+
+    json_data = request.json
+    handler = request.headers
+    qs = request.args
+    print(f'{json_data=}, {handler=}, {qs=}')
+
     response = jsonify({'Hello': 'world'})
     print(response)
     return response
 
 
-app.add_url_rule('/hello/world', view_func=hello_world, methods=['POST'])
+app.add_url_rule('/hello/world/<int:some_id>', view_func=hello_world, methods=['POST'])
 
 
 if __name__ == '__main__':
